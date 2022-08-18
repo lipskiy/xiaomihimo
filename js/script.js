97238dafdfcc4148106f26d2fc567182f45ef523
Alexander <lipskiypro@gmail.com>
2022-08-18 19:14:48 +0300

    footer and scroll id

--- js/script.js
+++ js/script.js
@@ -6,4 +6,18 @@ $(function(){
       autoplay: true,
       autoplaySpeed: 3000
    });
+
+   $(".menu, .bike__column").on("click","a", function (event) {
+		//отменяем стандартную обработку нажатия по ссылке
+		event.preventDefault();
+
+		//забираем идентификатор блока с атрибута href
+		var id  = $(this).attr('href'),
+
+		//узнаем высоту от начала страницы до блока на который ссылается якорь
+			top = $(id).offset().top;
+		
+		//анимируем переход на расстояние - top за 1500 мс
+		$('body,html').animate({scrollTop: top}, 1500);
+	});
 });
